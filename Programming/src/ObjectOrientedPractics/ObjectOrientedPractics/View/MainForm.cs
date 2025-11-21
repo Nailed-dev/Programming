b44a6d1d4f7fcf9be5d914dbdf2d659446e66154
Nailed-dev <152404010+Nailed-dev@users.noreply.github.com>
2025-11-21 19:48:01 +0700

    Add files via upload

--- Programming/src/ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.cs
+++ Programming/src/ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.cs
@@ -10,10 +10,7 @@ namespace ObjectOrientedPractics
         public MainForm()
         {
             InitializeComponent();
-            var customer = new Customer();
-            customer.Fullname = "Brandon Vicle Jhon";
             
-            MessageBox.Show(customer.Id.ToString());
             
 
 
