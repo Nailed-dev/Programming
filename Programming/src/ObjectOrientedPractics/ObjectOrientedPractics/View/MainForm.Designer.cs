30f9b9dc9a3689cb879846944c26f604dec34283
Nailed-dev <152404010+Nailed-dev@users.noreply.github.com>
2025-11-21 19:46:34 +0700

    Начал прописывать логику

--- Programming/src/ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.Designer.cs
+++ Programming/src/ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            itemTab1 = new ObjectOrientedPractics.View.Tabs.ItemTab();
+            tabPage2 = new TabPage();
+            customerTab1 = new ObjectOrientedPractics.View.Tabs.CustomerTab();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1349, 1148);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(itemTab1);
+            tabPage1.Location = new Point(4, 39);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1341, 1105);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Items";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // itemTab1
+            // 
+            itemTab1.Dock = DockStyle.Fill;
+            itemTab1.Location = new Point(3, 3);
+            itemTab1.Name = "itemTab1";
+            itemTab1.Size = new Size(1335, 1099);
+            itemTab1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(customerTab1);
+            tabPage2.Location = new Point(4, 39);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1341, 1105);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Customer";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // customerTab1
+            // 
+            customerTab1.Dock = DockStyle.Fill;
+            customerTab1.Location = new Point(3, 3);
+            customerTab1.Name = "customerTab1";
+            customerTab1.Size = new Size(1335, 1099);
+            customerTab1.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1349, 1148);
+            Controls.Add(tabControl1);
+            Name = "MainForm";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage2.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private View.Tabs.ItemTab itemTab1;
+        private TabPage tabPage2;
+        private View.Tabs.CustomerTab customerTab1;
     }
 }
