48cf7cf4edd3b7c9c602e4159005ada5b1f9ec74
Nailed-dev <152404010+Nailed-dev@users.noreply.github.com>
2025-11-23 20:17:26 +0700

    Add files via upload

---
+++ Programming/src/ObjectOrientedPractics/ObjectOrientedPractics/View/Tabs/CustomersTab.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ObjectOrientedPractics.Model;
+using ObjectOrientedPractics.Services;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ObjectOrientedPractics.View.Tabs
+{
+    public partial class CustomersTab : UserControl
+    {
+        /// <summary>
+        /// Список всех пользователей.
+        /// </summary>
+        private List<Customer> _customers = new List<Customer>();
+
+        /// <summary>
+        /// Выбранный пользователь.
+        /// </summary>
+        private Customer _currentCustomer;
+
+        public CustomersTab()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Очистка тексбоксов.
+        /// </summary>
+        private void ClearTextBoxes()
+        {
+            IdTextBox.Clear();
+            FullNameTextBox.Clear();
+            AddressTextBox.Clear();
+            IdTextBox.BackColor = Colors.NormalColor;
+            FullNameTextBox.BackColor = Colors.NormalColor;
+            AddressTextBox.BackColor = Colors.NormalColor;
+        }
+
+        /// <summary>
+        /// Обновление текстбоксов в зависимости от выбранного пользователя.
+        /// </summary>
+        /// <param name="customer">Пользователь</param>
+        private void UpdateTextBoxes(Customer customer)
+        {
+            IdTextBox.Text = customer.Id.ToString();
+            FullNameTextBox.Text = customer.Fullname;
+            AddressTextBox.Text = customer.Address;
+        }
+
+        /// <summary>
+        /// Добавление нового пользователя.
+        /// </summary>
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            _currentCustomer = new Customer("Empy user", "Empty address");
+            _customers.Add(_currentCustomer);
+            CustomersListBox.Items.Add(_currentCustomer.Fullname);
+            CustomersListBox.SelectedIndex = _customers.Count - 1;
+            UpdateTextBoxes(_currentCustomer);
+        }
+
+        /// <summary>
+        /// Удаление пользователя.
+        /// </summary>
+        private void RemoveButton_Click(object sender, EventArgs e)
+        {
+            if (CustomersListBox.Items.Count == 0) return;
+            int index = CustomersListBox.SelectedIndex;
+            _customers.RemoveAt(index);
+            CustomersListBox.Items.RemoveAt(index);
+
+            if (CustomersListBox.Items.Count > index)
+                CustomersListBox.SelectedIndex = index;
+            else if (CustomersListBox.Items.Count != 0)
+                CustomersListBox.SelectedIndex = 0;
+            else
+                CustomersListBox.SelectedIndex = -1;
+
+            UpdateTextBoxes(_currentCustomer);
+
+            if (CustomersListBox.Items.Count == 0)
+                ClearTextBoxes();
+        }
+
+        /// <summary>
+        /// Изменение имени пользователя, когда изменяется имя в FullNameTextBox.
+        /// </summary>
+        private void FullNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (_customers.Count == 0) return;
+
+            try
+            {
+                FullNameTextBox.BackColor = Colors.NormalColor;
+                _currentCustomer.Fullname = FullNameTextBox.Text;
+                CustomersListBox.Items[_customers.IndexOf(_currentCustomer)] = _currentCustomer.Fullname;
+            }
+            catch
+            {
+
+                FullNameTextBox.BackColor = Colors.ErrorColor;
+            }
+        }
+
+        /// <summary>
+        /// Изменение адреса пользователя, когда изменяется адрес в AddressTextBox.
+        /// </summary>
+        private void AddressTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (_customers.Count == 0) return;
+
+            try
+            {
+                AddressTextBox.BackColor = Colors.NormalColor;
+                _currentCustomer.Address = AddressTextBox.Text;
+            }
+            catch
+            {
+                AddressTextBox.BackColor = Colors.ErrorColor;
+            }
+        }
+
+        /// <summary>
+        /// Обновление данных при выборе пользователя в CustomersListBox.
+        /// </summary>
+        private void CustomersListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = CustomersListBox.SelectedIndex;
+            if (index == -1) return;
+            _currentCustomer = _customers[index];
+            UpdateTextBoxes(_currentCustomer);
+        }
+
+        
+    }
+}
---
+++ Programming/src/ObjectOrientedPractics/ObjectOrientedPractics/View/Tabs/ItemsTab.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace ObjectOrientedPractics.View.Tabs
+{
+    partial class ItemsTab
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            panel1 = new Panel();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            AddButton = new Button();
+            RemoveButton = new Button();
+            ItemsListBox = new ListBox();
+            label1 = new Label();
+            panel2 = new Panel();
+            ItemCategoryComboBox = new ComboBox();
+            label7 = new Label();
+            DescriptionTextBox = new TextBox();
+            label6 = new Label();
+            NameTextBox = new TextBox();
+            label5 = new Label();
+            CostTextBox = new TextBox();
+            IdTextBox = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            tableLayoutPanel1.SuspendLayout();
+            panel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 40F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 60F));
+            tableLayoutPanel1.Controls.Add(panel1, 0, 0);
+            tableLayoutPanel1.Controls.Add(panel2, 1, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new Size(1300, 1100);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(tableLayoutPanel2);
+            panel1.Controls.Add(ItemsListBox);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(514, 1094);
+            panel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel2.ColumnCount = 3;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel2.Controls.Add(AddButton, 0, 0);
+            tableLayoutPanel2.Controls.Add(RemoveButton, 1, 0);
+            tableLayoutPanel2.Location = new Point(3, 986);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 1;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel2.Size = new Size(508, 105);
+            tableLayoutPanel2.TabIndex = 2;
+            // 
+            // AddButton
+            // 
+            AddButton.Dock = DockStyle.Fill;
+            AddButton.Location = new Point(3, 3);
+            AddButton.Name = "AddButton";
+            AddButton.Size = new Size(163, 99);
+            AddButton.TabIndex = 0;
+            AddButton.Text = "Add";
+            AddButton.UseVisualStyleBackColor = true;
+            AddButton.Click += AddButton_Click;
+            // 
+            // RemoveButton
+            // 
+            RemoveButton.Dock = DockStyle.Fill;
+            RemoveButton.Location = new Point(172, 3);
+            RemoveButton.Name = "RemoveButton";
+            RemoveButton.Size = new Size(163, 99);
+            RemoveButton.TabIndex = 1;
+            RemoveButton.Text = "Remove";
+            RemoveButton.UseVisualStyleBackColor = true;
+            RemoveButton.Click += RemoveButton_Click_1;
+            // 
+            // ItemsListBox
+            // 
+            ItemsListBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            ItemsListBox.BorderStyle = BorderStyle.FixedSingle;
+            ItemsListBox.FormattingEnabled = true;
+            ItemsListBox.IntegralHeight = false;
+            ItemsListBox.ItemHeight = 30;
+            ItemsListBox.Location = new Point(3, 39);
+            ItemsListBox.Name = "ItemsListBox";
+            ItemsListBox.Size = new Size(508, 934);
+            ItemsListBox.TabIndex = 1;
+            ItemsListBox.SelectedIndexChanged += ItemsListBox_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Items";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.White;
+            panel2.Controls.Add(ItemCategoryComboBox);
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(DescriptionTextBox);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(NameTextBox);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(CostTextBox);
+            panel2.Controls.Add(IdTextBox);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(label2);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(523, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(774, 1094);
+            panel2.TabIndex = 1;
+            // 
+            // ItemCategoryComboBox
+            // 
+            ItemCategoryComboBox.BackColor = Color.White;
+            ItemCategoryComboBox.FormattingEnabled = true;
+            ItemCategoryComboBox.Location = new Point(131, 195);
+            ItemCategoryComboBox.Name = "ItemCategoryComboBox";
+            ItemCategoryComboBox.Size = new Size(218, 38);
+            ItemCategoryComboBox.TabIndex = 10;
+            ItemCategoryComboBox.SelectedIndexChanged += ItemCategoryComboBox_SelectedIndexChanged;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(3, 198);
+            label7.Name = "label7";
+            label7.Size = new Size(101, 30);
+            label7.TabIndex = 9;
+            label7.Text = "Category:";
+            // 
+            // DescriptionTextBox
+            // 
+            DescriptionTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            DescriptionTextBox.BorderStyle = BorderStyle.FixedSingle;
+            DescriptionTextBox.Location = new Point(3, 626);
+            DescriptionTextBox.MaximumSize = new Size(1100, 1200);
+            DescriptionTextBox.Multiline = true;
+            DescriptionTextBox.Name = "DescriptionTextBox";
+            DescriptionTextBox.Size = new Size(768, 220);
+            DescriptionTextBox.TabIndex = 8;
+            DescriptionTextBox.TextChanged += DescriptionTextBox_TextChanged;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(3, 593);
+            label6.Name = "label6";
+            label6.Size = new Size(118, 30);
+            label6.TabIndex = 7;
+            label6.Text = "Description";
+            // 
+            // NameTextBox
+            // 
+            NameTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            NameTextBox.BorderStyle = BorderStyle.FixedSingle;
+            NameTextBox.Location = new Point(3, 319);
+            NameTextBox.MaximumSize = new Size(1100, 1200);
+            NameTextBox.Multiline = true;
+            NameTextBox.Name = "NameTextBox";
+            NameTextBox.Size = new Size(768, 269);
+            NameTextBox.TabIndex = 6;
+            NameTextBox.TextChanged += NameTextBox_TextChanged;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(3, 286);
+            label5.Name = "label5";
+            label5.Size = new Size(74, 30);
+            label5.TabIndex = 5;
+            label5.Text = "Name:";
+            // 
+            // CostTextBox
+            // 
+            CostTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            CostTextBox.BorderStyle = BorderStyle.FixedSingle;
+            CostTextBox.Location = new Point(131, 136);
+            CostTextBox.MaximumSize = new Size(600, 600);
+            CostTextBox.Name = "CostTextBox";
+            CostTextBox.Size = new Size(218, 35);
+            CostTextBox.TabIndex = 4;
+            CostTextBox.TextChanged += CostTextBox_TextChanged;
+            // 
+            // IdTextBox
+            // 
+            IdTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            IdTextBox.BackColor = Color.White;
+            IdTextBox.BorderStyle = BorderStyle.FixedSingle;
+            IdTextBox.Location = new Point(131, 74);
+            IdTextBox.MaximumSize = new Size(500, 500);
+            IdTextBox.Name = "IdTextBox";
+            IdTextBox.ReadOnly = true;
+            IdTextBox.Size = new Size(218, 35);
+            IdTextBox.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(3, 138);
+            label4.Name = "label4";
+            label4.Size = new Size(59, 30);
+            label4.TabIndex = 2;
+            label4.Text = "Cost:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(3, 76);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 30);
+            label3.TabIndex = 1;
+            label3.Text = "ID:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label2.Location = new Point(3, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(153, 30);
+            label2.TabIndex = 0;
+            label2.Text = "Selected Item";
+            // 
+            // ItemsTab
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(tableLayoutPanel1);
+            Name = "ItemsTab";
+            Size = new Size(1300, 1100);
+            tableLayoutPanel1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            tableLayoutPanel2.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Panel panel1;
+        private Label label1;
+        private Panel panel2;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Button AddButton;
+        private Button RemoveButton;
+        private ListBox ItemsListBox;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private TextBox CostTextBox;
+        private TextBox IdTextBox;
+        private TextBox DescriptionTextBox;
+        private Label label6;
+        private TextBox NameTextBox;
+        private Label label5;
+        private ComboBox ItemCategoryComboBox;
+        private Label label7;
+    }
+}
